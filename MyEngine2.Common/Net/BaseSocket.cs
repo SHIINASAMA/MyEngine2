a9d365bd78c379045cd3b641af7dfb8bfb3b5c97
Kaoru <SHIINA_KAORU@outlook.com>
2021-12-06 10:45:28 +0800

    为满足后续需求，设计了 BaseSocket

---
+++ MyEngine2.Common/Net/BaseSocket.cs
@@ -0,0 +1,81 @@
+﻿using System.Text;
+using System.Net;
+using System.Net.Sockets;
+
+namespace MyEngine2.Common.Net
+{
+    public class BaseSocket : Socket
+    {
+        public int MaxHeaderLength { get; } = 1024 * 80;
+
+        public BaseSocket(AddressFamily addressFamily, int maxHeaderLength = 1024 * 80) : base(addressFamily, SocketType.Stream, ProtocolType.Tcp)
+        {
+            MaxHeaderLength = maxHeaderLength;
+        }
+
+        public BaseSocket(SafeSocketHandle safeSocketHandle) : base(safeSocketHandle)
+        {
+        }
+
+        public void Bind(IPAddress iPAddress, int port)
+        {
+            Bind(new IPEndPoint(iPAddress, port));
+        }
+
+        public new BaseSocket Accept()
+        {
+            return new BaseSocket(base.Accept().SafeHandle);
+        }
+
+        public string ReadLine()
+        {
+            byte[] buffer = new byte[1];
+            char ch;
+            bool hit = false;// 上一个字符是否为 '\r'
+            StringBuilder stringBuilder = new StringBuilder();
+
+            int count = 0;
+            while (true)
+            {
+                Receive(buffer, 1, SocketFlags.None);
+                ch = Encoding.ASCII.GetString(buffer)[0];
+                count++;
+                if (count >= MaxHeaderLength)
+                {
+                    throw new HttpHeaderException("Http 头部单行过长");
+                }
+                else if (ch == '\r')
+                {
+                    hit = true;
+                    continue;
+                }
+                else if (ch == '\n')
+                {
+                    if (hit)
+                    {
+                        return stringBuilder.ToString();
+                    }
+                    else
+                    {
+                        stringBuilder.Append(ch);
+                        hit = false;
+                    }
+                }
+                else
+                {
+                    stringBuilder.Append(ch);
+                    continue;
+                }
+            }
+        }
+
+        public int WriteLine(string line)
+        {
+            if (line.Length > MaxHeaderLength + 2)
+            {
+                throw new HttpHeaderException("Http 头部单行过长");
+            }
+            return base.Send(Encoding.ASCII.GetBytes(line + "\r\n"));
+        }
+    }
+}
---
+++ MyEngine2.Test.Common/TestNet.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MyEngine2.Common.Logger;
+using MyEngine2.Common.Net;
+
+namespace MyEngine2.Test.Common
+{
+    [TestClass]
+    public class TestNet
+    {
+        [TestMethod]
+        public void TestBaseSocket()
+        {
+            Logger logger = new Logger();
+            logger.AddAppender(new ConsoleAppender(new Formatter()));
+
+            BaseSocket serverSocket = new BaseSocket(System.Net.Sockets.AddressFamily.InterNetwork);
+            serverSocket.Bind(System.Net.IPAddress.Loopback, 8080);
+            serverSocket.Listen(10);
+
+            BaseSocket clientSocket = serverSocket.Accept();
+            while (true)
+            {
+                string line = clientSocket.ReadLine();
+                if (line.Length != 0)
+                {
+                    // System.Console.WriteLine(line);
+                    LoggerManager.Logger.Info(line);
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            clientSocket.WriteLine("HTTP/1.1 302");
+            clientSocket.WriteLine("Location: www.baidu.com");
+            clientSocket.WriteLine("");
+            clientSocket.Shutdown(System.Net.Sockets.SocketShutdown.Both);
+            clientSocket.Close();
+            clientSocket.Dispose();
+            //serverSocket.Shutdown(System.Net.Sockets.SocketShutdown.Both);
+            //serverSocket.Close();
+            //serverSocket.Dispose();
+        }
+    }
+}
