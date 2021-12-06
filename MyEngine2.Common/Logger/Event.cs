19de9cfea0c02d2def1d7f36ce1fc032b575a54e
Kaoru <SHIINA_KAORU@outlook.com>
2021-12-06 18:00:53 +0800

    修改字段拼写

--- MyEngine2.Common/Logger/Event.cs
+++ MyEngine2.Common/Logger/Event.cs
@@ -4,15 +4,15 @@ namespace MyEngine2.Common.Logger
 {
     public class Event
     {
-        private static string UNKNOWN_FILE = "Unknown_File";
+        private static string UnknownFile = "Unknown_File";
 
-        private static string UNKNOWN_THREAD = "Unknown_Thread";
+        private static string UnknownThread = "Unknown_Thread";
 
         public Level Level { get; }
         public DateTime Time { get; }
 
         public string FullFileName
-        { get { return StackFrame.GetFileName() ?? UNKNOWN_FILE; } }
+        { get { return StackFrame.GetFileName() ?? UnknownFile; } }
 
         public string FileName
         {
@@ -30,7 +30,7 @@ namespace MyEngine2.Common.Logger
                 }
                 else
                 {
-                    return UNKNOWN_FILE;
+                    return UnknownFile;
                 }
             }
         }
@@ -39,7 +39,7 @@ namespace MyEngine2.Common.Logger
         { get { return StackFrame.GetFileLineNumber(); } }
 
         public string ThreadName
-        { get { return Thread.Name ?? UNKNOWN_THREAD; } }
+        { get { return Thread.Name ?? UnknownThread; } }
 
         public int ThreadId
         { get { return Thread.ManagedThreadId; } }
